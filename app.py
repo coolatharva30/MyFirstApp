d93c8e2ed5dfa49fe078b4e48c9493fe5b1efbcc
Atharva Gangapure <142802749+coolatharva30@users.noreply.github.com>
2025-02-04 23:31:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pickle
 
-model = pickle.load(open("placement_model.pkl","rb"))
+model = pickle.load(open("placement_model.pkl","rb")
 
 st.set_page_config(page_title="Student Placement Predictor")
 
